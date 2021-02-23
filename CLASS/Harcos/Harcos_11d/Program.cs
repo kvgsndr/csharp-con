4fd5bf730cc628c87b8485a525bfd50a20bac72e
Kővágó Sándor <56760241+kvgsndr@users.noreply.github.com>
2021-02-23 12:32:53 +0100

    20210223

--- CLASS/Harcos/Harcos_11d/Program.cs
+++ CLASS/Harcos/Harcos_11d/Program.cs
@@ -6,7 +6,16 @@ namespace Harcos_11d
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Harcos h1 = new Harcos("Laci", 100, 5);
+            Harcos h2 = new Harcos("Kati", 100, 4);
+
+            while (!h1.harcol(h2))
+            {
+                h1.toString();
+                h2.toString();
+            }
+            Console.ReadKey();
+
         }
     }
 }
