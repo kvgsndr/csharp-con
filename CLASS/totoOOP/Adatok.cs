826ba3707ce01e0b4cc6380586942cccbb2b388f
Kővágó Sándor <56760241+kvgsndr@users.noreply.github.com>
2021-03-24 11:40:19 +0100

    20210324

---
+++ CLASS/totoOOP/Adatok.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace totoOOP
+{
+    class Adatok
+    {
+        int ev;
+        int het;
+        int fordulo;
+        int t13p1;
+        int ny13p1;
+        string eredmeny;
+        public Adatok(int ev, int het, int fordulo, int t13p1, int ny13p1, string eredmeny )
+        {
+            this.ev= ev;
+            this.het=het;
+            this.fordulo=fordulo;
+            this.T13p1=t13p1;
+            this.ny13p1=ny13p1;
+            this.eredmeny=eredmeny;
+
+        }
+
+        public int T13p1 { get => t13p1; set => t13p1 = value; }
+        public long kifizetve(){ return (long)t13p1 * ny13p1; }
+    }
+}
---
+++ CLASS/totoOOP/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace totoOOP
+{
+    class Program
+    {
+        static public void fel(int n)
+        { Console.Write($"Feladat {n}:"); }
+
+        static void Main(string[] args)
+        {
+            List<Adatok> totoList = new List<Adatok>();
+            StreamReader fbe = new StreamReader("toto.txt");
+            fbe.ReadLine();
+            while (!fbe.EndOfStream)
+            {
+                string sor = fbe.ReadLine();
+                var elemek = sor.Split(';');
+                totoList.Add(new Adatok(Int32.Parse(elemek[0]), Int32.Parse(elemek[1]), Int32.Parse(elemek[2]), Int32.Parse(elemek[3]), Int32.Parse(elemek[4]), elemek[5]));
+            }
+            fbe.Close();
+
+            fel(3);
+            Console.WriteLine($"A fordulók száma : {totoList.Count}");
+            int telital = 0;
+            long fizetve = 0;
+            foreach (var item in totoList)
+            {
+                telital += item.T13p1;
+                fizetve += item.kifizetve();
+            }
+            fel(4);
+            Console.WriteLine($"A telitalálatos szelvények száma: {telital} db.");
+
+            Console.ReadKey();
+        }
+    }
+}
